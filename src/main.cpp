d5430f3993a07fa2f4e84a848ef2b8dfe5931c92
JosepUPC <github.jeb@gmail.com>
2023-03-08 12:19:28 +0100

    Master_first_exercise

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+void setup()
+{
+    Serial.begin(112500);
 
-void loop() {
-  // put your main code here, to run repeatedly:
+    /* we create a new task here */
+    xTaskCreate
+    (
+      anotherTask, /* Task function. */
+      "another Task", /* name of task. */
+      10000, /* Stack size of task */
+      NULL, /* parameter of the task */
+      1, /* priority of the task */
+      NULL /* Task handle to keep track of created task */
+      ); 
+}
+ 
+/* the forever loop() function is invoked by Arduino ESP32 loopTask */
+void loop()
+{
+  Serial.println("this is ESP32 Task");
+  delay(1000);
+}
+ 
+/* this function will be invoked when additionalTask was created */
+void anotherTask( void * parameter )
+{
+  
+  /* loop forever */
+  for(;;)
+  {
+    Serial.println("this is another Task");
+    delay(1000);
+  }
+
+  /* delete a task when finish,
+  this will never happen because this is infinity loop */
+  vTaskDelete( NULL );
 }
